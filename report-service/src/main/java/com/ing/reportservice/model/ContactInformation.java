08ad3cdd2ff6ba4255a55c472be5086f3fd90ccb
clfv5 <bedirhanakcay@icloud.com>
2021-10-04 00:41:45 +0300

    contact information entity added

---
+++ report-service/src/main/java/com/ing/reportservice/model/ContactInformation.java
@@ -0,0 +1,48 @@
+package com.ing.reportservice.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ContactInformation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    public ContactInformation() {
+        super();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactInformation [email=" + email + ", phone=" + phone + "]";
+    }
+
+}
