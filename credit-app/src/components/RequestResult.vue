cd1e13b7b0b4305b4983b49d869608ffaccb7cbf
clfv5 <bedirhanakcay@icloud.com>
2021-10-04 15:47:02 +0300

    RequestResult page added

---
+++ credit-app/src/components/RequestResult.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="result" :class="{ danger: background}">
+    <div class="message">
+      <slot />
+    </div>
+    <button class="button" @click="onClick">Forma Dön</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["background"],
+  methods: {
+    onClick() {
+      this.$emit("reset");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.result{
+    display: inline-block;
+    position: relative;
+    background: #ACE1AF;
+    height: 10rem;
+    text-align: center;
+    border-radius: 1rem;
+}
+
+.danger {
+    background: #e57373;
+}
+
+.message {
+    font-size: 1.5rem;
+    padding: 1rem;
+}
+
+.button {
+    border: none;
+    position: absolute;
+    bottom: 15px;
+    left: 40%;
+    border-radius: 0.45rem;
+    background: #0074D9;
+    size: 2rem;
+    padding: 15px 32px;
+    color: white;
+
+}
+</style>
